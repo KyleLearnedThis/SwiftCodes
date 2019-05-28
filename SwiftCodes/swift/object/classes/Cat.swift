523c3fa92f995f5f139a5a5d9fcf9291fa9cc019
KyleLearnedThis <kyle.learned.this@gmail.com>
2019-05-28 01:54:05 -0700

    Proctocol, Sub-class, extension testing. (#25)

---
+++ SwiftCodes/swift/object/classes/Cat.swift
@@ -0,0 +1,14 @@
+//
+//  Cat.swift
+//  SwiftCodes
+//
+//  Copyright © 2019 kylelearnedthis. All rights reserved.
+//
+
+import Foundation
+
+class Cat: Sound {
+    func makeSound() {
+        print("======== MEOW! ========")
+    }
+}
