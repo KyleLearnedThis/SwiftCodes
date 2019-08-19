fc2954893ac7d89e78e34dbcc28df81f8a4f5167
KyleLearnedThis <kyle.learned.this@gmail.com>
2019-08-19 00:39:04 -0700

    SortByMember code refactoring (#33)

---
+++ SwiftCodes/swift/object/classes/data/Pair.swift
@@ -0,0 +1,17 @@
+//
+//  Pair.swift
+//  SwiftCodes
+//
+//  Copyright © 2019 kylelearnedthis. All rights reserved.
+//
+
+import Foundation
+
+open class Pair {
+    var key: Int
+    var value: String
+    init(_ k: Int,_ v: String) {
+        key = k
+        value = v
+    }
+}
