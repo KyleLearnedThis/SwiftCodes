f181f40990c749e4e14f796286b79525a293e0c0
KyleLearnedThis <kyle.learned.this@gmail.com>
2020-06-23 02:13:36 -0700

    Closure example and basic test (#45)

---
+++ SwiftCodes/swift/closures/ComputeClosure.swift
@@ -0,0 +1,24 @@
+//
+//  CustomClosure.swift
+//  SwiftCodes
+//
+//  Created by KyleLearnedThis on 6/23/20.
+//  Copyright © 2020 kylelearnedthis. All rights reserved.
+//
+
+import Foundation
+
+class ComputeClosure {
+    var result = 0
+
+    func addOne(input: Int, closure: (Int)->Int ) {
+        result = closure(input)
+        result += 1
+        print("Step 2: result is \(result)")
+    }
+
+    func greet(name: String, closure: () -> ()) {
+        print("Hello, \(name)!")
+        closure()
+    }
+}
