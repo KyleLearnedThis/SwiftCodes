77e89adc0c263a64908f857249db9d798b8a03b6
KyleLearnedThis <kyle.learned.this@gmail.com>
2021-08-03 14:12:27 -0700

    DetectCyclicList Solution and test (#75)

---
+++ SwiftCodes/list/DetectCyclicList.swift
@@ -0,0 +1,27 @@
+//
+//  DetectCyclicList.swift
+//  SwiftCodes
+//
+//  Created by KyleLearnedThis on 8/3/21.
+//  Copyright © 2021 kylelearnedthis. All rights reserved.
+//
+
+import SwiftDS
+
+class DetectCyclicList {
+    static func hasCycle(node: Node<Int>? ) -> Bool {
+        if node == nil || node?.next == nil {
+            return false
+        }
+        var slow = node
+        var fast = node?.next
+        while slow != fast {
+            if fast == nil || fast?.next == nil {
+                return false
+            }
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+        return true
+    }
+}
