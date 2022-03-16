4983cb1c3459b5c3969bdbd17c9d753031c86fbd
KyleLearnedThis <kyle.learned.this@gmail.com>
2022-03-15 17:34:28 -0700

    Another simple way to solve Palindrome with test

--- SwiftCodes/classics/Palindrome.swift
+++ SwiftCodes/classics/Palindrome.swift
@@ -16,4 +16,20 @@ class Palindrome {
         let revsersed = String(input.reversed())
         return revsersed == input
     }
+
+    static func isPalindromeV2(_ s: String) -> Bool {
+        let pattern = "[^A-Za-z0-9]+"
+        let input = s.replacingOccurrences(of: pattern, with: "", options: [.regularExpression])
+            .lowercased()
+        let arr = Array(input)
+        let size = arr.count
+        for i in 0 ... (size / 2) {
+            let l = arr[i]
+            let r = arr[size - i - 1]
+            if l != r {
+                return false
+            }
+        }
+        return true
+    }
 }
